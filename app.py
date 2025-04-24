7bd5fde921ee7cd7e9cd1efef36e7aba005d82c5
juergen-geiser <141345657+juergen-geiser@users.noreply.github.com>
2025-04-24 14:26:34 +0200

    Update app.py
    Simplification of the file-app

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def classify():
 
 if __name__ == "__main__":
     # Nur hier wird der Server gestartet – einmalig!
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    #app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
